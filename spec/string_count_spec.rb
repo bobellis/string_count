d422d45cbf7e04dfa8323df9443e4ee1efa3a7ea
bobellis <bobellis.tx@gmail.com>
2015-04-24 09:46:41 -0700

    added Gemfile.lock after running bundle install

---
+++ spec/string_count_spec.rb
@@ -0,0 +1,11 @@
+require ('rspec')
+require ('string_count.rb')
+
+describe('String#string_count') do
+  it('it takes a target word and the same word as the string to check and returns 1') do
+    expect('circus'.string_count('circus')).to(eq(1))
+  end
+
+  #it("takes a target word and a string then counts the number of times the target word appears in the string") do
+  #end
+end
