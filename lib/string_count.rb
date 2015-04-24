9f5ccf1ba91b64d35c49346c899c2e416e6e0928
bobellis <bobellis.tx@gmail.com>
2015-04-24 09:53:59 -0700

    added code to pass first spec

--- lib/string_count.rb
+++ lib/string_count.rb
@@ -1,6 +1,13 @@
 class String
-  define_method(:check_string) do |string_to_check|
-
-
+  define_method(:count_words) do |words_to_check|
+    target_word = self.strip()
+    count = 0
+    words_to_check = words_to_check.split()
+    words_to_check.each do |word|
+      if word = target_word
+        count = count + 1
+      end
+    end
+    count
   end
 end
